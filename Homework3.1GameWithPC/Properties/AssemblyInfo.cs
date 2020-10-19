cc4ba1a6d23baf4b9d7c88f65a8d0b8a717af9bc
Виктор <windem4@gmail.com>
2020-10-19 10:16:37 +0300

    Добавьте файлы проекта.

---
+++ Homework3.1GameWithPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Homework3.1GameWithPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Homework3.1GameWithPC")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7d8404b0-5ba3-4b6c-82b0-2213822d1eb1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(100, 40); // задание размера консоли
+
+            // Вывод правил игры
+            Console.ForegroundColor = ConsoleColor.DarkBlue; // Задание цвета выводимого текста
+            Console.Write($"{ "Правила игры:\n",60}" +
+                $"{"Случайным образом генерируется число от 12 до 120\n",77}" +
+                 $"{"Каждый игрок по очереди вводит число от 1 до 4\n",76}" +
+                $"{"Побеждает игрок первым достигший нуля.\n",69}" +
+                $"{"Если последний результат будет отрицательным\n",73}" +
+                $"{"то будет сгенерированно случайное число от 10 до 50\n",78}" + 
+                $"{"и ход перейдет к следующему игроку\n",68}");
+
+            Console.WriteLine(" "); // Пустая строка для более удобного визуального восприятия
+
+            int maxNumber = 4; // Максимальное вводимое число
+            int minNumber = 1; // Минимально вводимое число
+
+            Random RandomNum = new Random(); // генератор случайных чисел
+            int gameNumber = RandomNum.Next(12, 121); // генерация случайного числа и запись его в переменную
+
+            int GamerNum; // переменные ввода игроков
+
+            Console.ForegroundColor = ConsoleColor.Gray; // Задание цвета выводимого текста
+
+            Console.Write($"{"Введите имя первого игрока: ",63}"); //
+            string Gamer1 = Console.ReadLine();                   // Ввод имени первого игрока
+
+            Console.Write($"{"Введите имя второго игрока: ",63}"); //
+            string Gamer2 = Console.ReadLine();                    // Ввод имени второго игрока
+
+            Console.WriteLine($"{ "Заданное число: " + gameNumber,58}" + "\n"); // Вывод случайно заданного числа
+
+            Console.WriteLine(" "); // Пустая строка для более удобного визуального восприятия
+
+            // Метод получения данных и выполнения условий первого игрока
+            bool Turn(int TempNum1, string TempGamer)
+            {
+                if (TempNum1 <= maxNumber && TempNum1 >= minNumber) // Условие соблюдения правил игры
+                {
+                    gameNumber = gameNumber - TempNum1; // вычисление разницы чисел первого игрока
+                }
+                else // Условие нарушения правил игры
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("\n" + $"{"Ошибка, ты играешь не по правилам! ход переходит к следующему игроку", 82}" + "\n");
+                    Console.ReadKey();
+                }
+                if (gameNumber == 0) // Условие победы первого игрока
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // Задание цвета выводимого текста
+
+                    Console.WriteLine($"{" Победил " + TempGamer + "! Поздравления!!! Игра будет закрыта. Нажми любую клавишу", 83}");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else if (gameNumber < minNumber) // Условие при отрицательном последнем значении
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray; // Задание цвета выводимого текста
+
+                    Console.Write($"{"Последний результат не может быть отрицательным! ",73}" + "\n" +
+                        $"{"Прибавляем случайное число от 10 до 50, и ход переходит к следующему игроку",88}" + "\n");
+                    gameNumber = gameNumber + RandomNum.Next(10, 51); // Генерация нового случайного числа
+                    Console.Write("\n" + $"{"новое число: " + gameNumber,56}" + "\n"); // Вывод нового случайного числа
+                }
+                return true; 
+            }
+            while (gameNumber > 0) // Цикл игры
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                Console.Write("Ходит: " + Gamer1 + "\nВведи число от 1 до 4\nТвое число: "); // 
+                int.TryParse(Console.ReadLine(), out GamerNum);                              // Ввод числа первого игрока
+
+                Turn(GamerNum, Gamer1); // метод вычисления разницы gameNumber и числа одного из игроков
+                Console.WriteLine($"{ "новое число: " + gameNumber,56}" + "\n"); // вывод в консоль разницы случайного числа и числа первого игрока.
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+                Console.Write("Ходит: " + Gamer2 + "\nВведи число от 1 до 4\nТвое число: "); // 
+                int.TryParse(Console.ReadLine(), out GamerNum);                              // Ввод числа первого игрока
+
+                Turn(GamerNum, Gamer2);
+                Console.WriteLine($"{ "новое число: " + gameNumber,56}" + "\n"); // вывод в консоль разницы случайного числа и числа первого игрока.   
+            }
+        }
+    }
+}
